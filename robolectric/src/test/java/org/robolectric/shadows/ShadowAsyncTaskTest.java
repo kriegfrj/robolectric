fc93c8b4ef2b0194b27080920aa359275a268da2
Fr Jeremy Krieg <fr.jeremy@stnectarios.org.au>
2015-10-17 02:30:09 +1030

    Removed some redundant delegate methods. Changed so that abort() stops progress callbacks. Added ability to capture & compare any exceptions that happen in doInBackground().

--- robolectric/src/test/java/org/robolectric/shadows/ShadowAsyncTaskTest.java
+++ robolectric/src/test/java/org/robolectric/shadows/ShadowAsyncTaskTest.java
@@ -15,14 +15,17 @@ import org.robolectric.util.ReflectionHelpers;
 import org.robolectric.util.Transcript;
 
 import java.util.ArrayDeque;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.robolectric.Shadows.shadowOf;
+import static org.robolectric.util.ReflectionHelpers.ClassParameter.from;
 
 @RunWith(TestRunners.MultiApiWithDefaults.class)
 public class ShadowAsyncTaskTest {
@@ -121,6 +124,17 @@ public class ShadowAsyncTaskTest {
 
     ShadowLooper.runUiThreadTasks();
     transcript.assertEventsSoFar("onPostExecute c");
+    assertThat(shadowOf(asyncTask).getBackgroundException()).as("bgException").isNull();
+  }
+
+  @Test
+  public void whenCancelled_withNoExecute_shouldRunPostExecute() {
+    MyAsyncTask task = new MyAsyncTask();
+
+    assertThat(task.cancel(true)).as("cancel").isTrue();
+    transcript.assertNoEventsSoFar();
+    ShadowLooper.runUiThreadTasks();
+    transcript.assertEventsSoFar("onCancelled null");
   }
 
   @Test
@@ -129,6 +143,7 @@ public class ShadowAsyncTaskTest {
 
     assertThat(asyncTask.cancel(true)).as("cancel").isTrue();
     assertThat(asyncTask.isCancelled()).as("isCancelled").isTrue();
+    transcript.assertNoEventsSoFar();
 
     asyncTask.execute("a", "b");
     transcript.assertEventsSoFar("onPreExecute");
@@ -139,6 +154,7 @@ public class ShadowAsyncTaskTest {
 
     ShadowLooper.runUiThreadTasks();
     transcript.assertEventsSoFar("onCancelled null");
+    assertThat(shadowOf(asyncTask).getBackgroundException()).as("bgException").isNull();
   }
 
   @Test
@@ -157,6 +173,7 @@ public class ShadowAsyncTaskTest {
 
     ShadowLooper.runUiThreadTasks();
     transcript.assertEventsSoFar("onCancelled null");
+    assertThat(shadowOf(asyncTask).getBackgroundException()).as("bgException").isNull();
   }
 
   @Test
@@ -175,6 +192,7 @@ public class ShadowAsyncTaskTest {
 
     ShadowLooper.runUiThreadTasks();
     transcript.assertEventsSoFar("onCancelled c");
+    assertThat(shadowOf(asyncTask).getBackgroundException()).as("bgException").isNull();
   }
 
   @Test
@@ -196,6 +214,7 @@ public class ShadowAsyncTaskTest {
 
     assertThat(asyncTask.cancel(true)).as("cancel").isFalse();
     assertThat(asyncTask.isCancelled()).as("isCancelled").isTrue();
+    assertThat(shadowOf(asyncTask).getBackgroundException()).as("bgException").isNull();
   }
 
   @Test
@@ -223,6 +242,45 @@ public class ShadowAsyncTaskTest {
         "onProgressUpdate 66%",
         "onProgressUpdate 99%",
         "onPostExecute done");
+  }
+
+  @Test
+  public void progressUpdates_shouldStopBeingQueued_onceAborted() throws Exception {
+    AsyncTask<String, String, String> asyncTask = new MyAsyncTask() {
+      @Override
+      protected String doInBackground(String... strings) {
+        publishProgress("33%");
+        shadowOf(this).abort();
+        publishProgress("66%");
+        publishProgress("99%");
+        return "done";
+      }
+    };
+
+    asyncTask.execute("a", "b");
+    transcript.assertEventsSoFar("onPreExecute");
+
+    ShadowApplication.runBackgroundTasks();
+    transcript.assertNoEventsSoFar();
+    assertThat(asyncTask.get(100, TimeUnit.MILLISECONDS)).as("Result should get stored in the AsyncTask").isEqualTo("done");
+
+    ShadowLooper.runUiThreadTasks();
+    transcript.assertEventsSoFar("onProgressUpdate 33%");
+  }
+
+  @Test(timeout=1000)
+  public void whenDoInBackgroundThrows_exceptionIsCaptured() {
+    final RuntimeException e = new RuntimeException("Hi there");
+    AsyncTask<String,String,String> task = new AsyncTask<String, String, String>() {
+      @Override
+      protected String doInBackground(String... params) {
+        throw e;
+      }
+    };
+
+    task.execute("a");
+    ShadowApplication.runBackgroundTasks();
+    assertThat(shadowOf(task).getBackgroundException().getCause()).isSameAs(e);
   }
 
   @Test
